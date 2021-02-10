b614843736ae324adf10076b6638474b8c20e2b3
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2021-02-10 15:13:54 -0600

    Set up menu with choice option to exit on the 4

--- assignment1.cpp
+++ assignment1.cpp
@@ -16,32 +16,40 @@ int main(){
     }
     
     int choice;
-    cout << "Select the actions:" << endl;
-    cout << "1) Display all flights" << endl;
-    cout << "2) Show the flights that depart from a given city" << endl;
-    cout << "3) Find a flight with the best distance to price ratio" << endl;
-    cout << "4) Exit the program" << endl
-}
+    while(choice !=4){
+        cout << "Select the actions:" << endl;
+        cout << "1) Display all flights" << endl;
+        cout << "2) Show the flights that depart from a given city" << endl;
+        cout << "3) Find a flight with the best distance to price ratio" << endl;
+        cout << "4) Exit the program" << endl;
+        cin >> choice;
+    }
 
-//int readTickets(){
+
+
+    return 0;
+}
+/*
+int readTickets(){
     // 1) “readTickets”. Given the file input stream, and empty array of 
     //flights, read the ticket file and return a size of an array.
 }
 
-//int printTickets(){
+int printTickets(){
     // 2) “printTickets”. Given the array of tickets and its size it prints those tickets in a human readable format.
 
 }
 
-//int printTicketsFromTo(){
+int printTicketsFromTo(){
     // 3) “pringTicketsFromTo”. Given the array of tickets, its size and a city 
     //name it prints flights from the given city.
 }
 
 
-//int findCheapestTicket(){
+int findCheapestTicket(){
     // 4) “findCheapestTicket”. Given the array of tickets, its size and “flightIndex” 
     //integer passed by reference it finds the highest dis- tance to price ratio, 
     //puts the index of the respective flight into “flightIndex” variable and returns the price. 
     //IMPORTANT: It should not print anything.
 }
+*/
