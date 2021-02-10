04c4ece92aa27cbf092589d1ee3115d61d73993e
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2021-02-10 14:27:20 -0600

    set up of functions

--- assignment1.cpp
+++ assignment1.cpp
@@ -8,3 +8,26 @@ using namespace std;
 int main(){
     
 }
+
+int readTickets(){
+    // 1) “readTickets”. Given the file input stream, and empty array of 
+    //flights, read the ticket file and return a size of an array.
+}
+
+int printTickets(){
+    // 2) “printTickets”. Given the array of tickets and its size it prints those tickets in a human readable format.
+
+}
+
+int printTicketsFromTo(){
+    // 3) “pringTicketsFromTo”. Given the array of tickets, its size and a city 
+    //name it prints flights from the given city.
+}
+
+
+int findCheapestTicket(){
+    // 4) “findCheapestTicket”. Given the array of tickets, its size and “flightIndex” 
+    //integer passed by reference it finds the highest dis- tance to price ratio, 
+    //puts the index of the respective flight into “flightIndex” variable and returns the price. 
+    //IMPORTANT: It should not print anything.
+}
